7530317ac97d4a0a156caa003675531e46b3f5c4
sulav97 <sulavkatuwal2@gmail.com>
2025-02-24 23:05:37 +0545

    working

---
+++ notes.js
@@ -0,0 +1,19 @@
+// Creating App for devices within IOS and Android Devices
+// Connecting the Device to determine the amount of calories and energy burned from exercise, walking, or any movementor even sleeping patterns
+// So to do so I will need to make the app capable of working on IOS 
+// Counting the macros from each data entry ✅
+// Using an APi to fetch grocery items and their calorie log ✅
+// Use the fetched data and add a button if the user chooses to add the item ✅
+// The user needs a profile page -- User's Name, age, weight, gender, height, activity level ✅
+// Counting the macros from each data entry ✅
+// Effects the macro calories for that day, have the user select the current calories from the calorie calculator (Button), these calories will then be deducted based on the calorie log ✅
+// Update the food search to allow user to change based on the number of servings or grams ✅
+// Ask for tips about how you want to change the website
+// AI might be a cool feature
+// Lets Develop an Organized Workout List based on the Muscle Group, possibly an icon for each muscle, which the user selects
+// Allowed the Users to Select Based on Categories Instead, this allows them to create any split they would like
+// Bug when logging an existing template workout it changes the logs to the with the same name to that exact logs info ✅
+// If they log the same workout multiple times on the same day it causes an issue as its unable to tell the difference apart. 
+// The categories that are created are given to all users, we must change it to specific users ✅
+// A bug for the deployed version whenever the user refreshes the app crashes, as in local host it would just go to home 
+// Somethings off with calorie log for getting the logs 
